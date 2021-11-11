0c095d7bf955a8a8548a0157d28c1a3a14c49890
MOUNIBMarouaneDWM <MOUNIBMarouaneDWM>
2021-11-11 13:49:52 +0100

    learn react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/header'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// IDEA: return div > h1 and h2
+const App=()=>{
+  return(
+    <>
+      <Header />
+    </>
+  )
+}
+
+
+// // IDEA: return h1 and h2
+// const Application=()=>(
+//   <React.Fragment>
+//     <h1>hello</h1>
+//     <h2>hello</h2>
+//   </React.Fragmen>
+//
+// )
+
+ReactDOM.render(<App />,document.getElementById('root'))
+// ReactDOM.render(<Application />,document.getElementById('root'))
